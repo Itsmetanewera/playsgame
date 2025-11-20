369a7092e9ff7236ffbe8147dd92fef91ca701f6
Itsmetanewera <m3tanewera@gmail.com>
2025-11-20 21:43:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo, Message
 from aiogram.filters import Command
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # на Railway задаём секрет
-ADMIN_IDS = {int(x) for x in os.getenv("ADMIN_IDS","").split(",") if x.strip().isdigit()}
+BOT_TOKEN = os.getenv("8521116610:AAE_aPCZ7KpXS2VPnPEtgYrHJ2oNMbNzJ70")  # на Railway задаём секрет
+ADMIN_IDS = {int(x) for x in os.getenv("6591391434").split(",") if x.strip().isdigit()}
 WEBAPP_URL = os.getenv("WEBAPP_URL","https://plays.io")  # указать plays.io
 
 bot = Bot(BOT_TOKEN)
